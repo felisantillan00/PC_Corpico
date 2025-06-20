f4983d61330edc57b073c6985ec3da75d93087cb
BaltaMoreno <baltamoreno444@gmail.com>
2025-06-20 20:14:48 -0300

    cambios en juego de encontrar

--- frontend/js/juegoEncontrar.js
+++ frontend/js/juegoEncontrar.js
@@ -1,187 +1,193 @@
-const moves = document.getElementById("moves-count");
-const tiempo = document.getElementById("time");
-const btnComenzar = document.getElementById("start");
-const btnDetener = document.getElementById("stop");
-const contenedorJuego = document.querySelector(".game-container");
-const resultado = document.getElementById("result");
-const controles = document.querySelector(".controls-container");
-let cartas;
-let intervalo;
-let primeraCarta = false;
-let seguntaCarta = false;
+document.addEventListener("DOMContentLoaded", () => {
+    const moves = document.getElementById("moves-count");
+    const tiempo = document.getElementById("time");
+    const btnDetener = document.getElementById("stop");
+    const contenedorJuego = document.querySelector(".game-container");
+    const resultado = document.getElementById("result");
+    const nombre = document.getElementById("nombre");
 
-const items = [
-    { name: "ayudarse", image: "../resources/img/Ayudarse.jpg" },
-    { name: "democracia", image: "../resources/img/Democracia.jpg" },
-    { name: "equidad", image: "../resources/img/equidad.jpg" },
-    { name: "honestidad", image: "../resources/img/Honestidad.jpeg" },
-    { name: "igualdad", image: "../resources/img/igualdad.jpeg" },
-    { name: "solidaridad", image: "../resources/img/solidaridad.jpg" },
-    { name: "responsabilidad", image: "../resources/img/responsabilidad.jpg" },
-    { name: "preocupacion", image: "../resources/img/preocupacionXlosDemas.jpg" },
-    { name: "responsabilidadSocial", image: "../resources/img/ResponsabilidadSocial.png" },
-    { name: "transparencia", image: "../resources/img/Transparencia.jpg" },
-    // { name: "", image: "" },
-];
+    let cartas;
+    let intervalo;
+    let primeraCarta = false;
+    let seguntaCarta = false;
 
-let segundos = 0, minutos = 0;
-let cantMovimientos = 0, victorias = 0;
+    const items = [
+        { name: "ayudarse", image: "../resources/img/Ayudarse.jpg" },
+        { name: "democracia", image: "../resources/img/Democracia.jpg" },
+        { name: "equidad", image: "../resources/img/equidad.jpg" },
+        { name: "honestidad", image: "../resources/img/Honestidad.jpeg" },
+        { name: "igualdad", image: "../resources/img/igualdad.jpeg" },
+        { name: "solidaridad", image: "../resources/img/solidaridad.jpg" },
+        { name: "responsabilidad", image: "../resources/img/responsabilidad.jpg" },
+        { name: "preocupacion", image: "../resources/img/preocupacionXlosDemas.jpg" },
+        { name: "responsabilidadSocial", image: "../resources/img/ResponsabilidadSocial.png" },
+        { name: "transparencia", image: "../resources/img/Transparencia.jpg" },
+        // { name: "", image: "" },
+    ];
 
-const timeGenerator = () => {
-    segundos += 1;
-    if (segundos >= 60) {
-        minutos += 1;
-        segundos = 0;
-    }
+    let segundos = 0, minutos = 0;
+    let cantMovimientos = 0, victorias = 0;
+    let puntos = 0;
 
-    let valorEnSegundos = segundos < 10 ? `0${segundos}` : segundos;
-    let valorEnMinutos = minutos < 10 ? `0${minutos}` : minutos;
-    tiempo.innerHTML = `<span>Time: </span>${valorEnMinutos}:${valorEnSegundos}`;
-};
-
-const contarMovimientos = () => {
-    cantMovimientos += 1;
-    moves.innerHTML = `<span>Moves:</span>${cantMovimientos}`;
-};
-
-//Pick random objects from the items array
-const generateRandom = (tamaño = 4) => {
-
-    let arrayTemp = [...items];
-
-    let valoresDeLasCartas = [];
-
-    tamaño = (tamaño * tamaño) / 2;
-
-    for (let i = 0; i < tamaño; i++) {
-        const randomIndex = Math.floor(Math.random() * arrayTemp.length);
-        valoresDeLasCartas.push(arrayTemp[randomIndex]);
-        arrayTemp.splice(randomIndex, 1);
-    }
-    return valoresDeLasCartas;
-};
-
-const generarMatriz = (valoresDeLasCartas, tamaño = 4) => {
-    contenedorJuego.innerHTML = "";
-
-    let primeraMitad = [];
-    let segundaMitad = [];
-
-    valoresDeLasCartas.forEach((carta) => {
-        primeraMitad.push({ ...carta, tipo: "imagen" });
-        segundaMitad.push({ ...carta, tipo: "texto" });
-    });
-    primeraMitad.sort(() => Math.random() - 0.5);
-    segundaMitad.sort(() => Math.random() - 0.5);
-
-    let valoresDistribuidos = [...primeraMitad, ...segundaMitad];
-
-    for (let i = 0; i < tamaño * tamaño; i++) {
-
-        if (i === 8) {
-            contenedorJuego.innerHTML += `
-            <div class="separador"></div>`
+    const timeGenerator = () => {
+        segundos += 1;
+        if (segundos >= 60) {
+            minutos += 1;
+            segundos = 0;
         }
-        const mitad = i < 8 ? "arriba" : "abajo";
-        contenedorJuego.innerHTML += `
-            <div class="card-container" data-card-value="${valoresDistribuidos[i].name}" data-mitad="${mitad}">
-                <div class="card-before">?</div>
-                <div class="card-after">
-                ${valoresDistribuidos[i].tipo === "imagen" 
-                    ? `<img src="${valoresDistribuidos[i].image}" class="image"/>` 
-                    : `<span class="palabra">${
-                        valoresDistribuidos[i].name === "responsabilidadSocial"
+
+        let valorEnSegundos = segundos < 10 ? `0${segundos}` : segundos;
+        let valorEnMinutos = minutos < 10 ? `0${minutos}` : minutos;
+        tiempo.innerHTML = "<span>Tiempo: </span>" + valorEnMinutos + ":" + valorEnSegundos;
+
+    };
+
+    const contarMovimientos = () => {
+        cantMovimientos += 1;
+        moves.innerHTML = `<span>Movimientos:</span>${cantMovimientos}`;
+    };
+
+    const generateRandom = (size = 4) => {
+        let arrayTemp = [...items];
+        let valoresDeLasCartas = [];
+
+        let cantidadNecesaria = (size * size) / 2;
+        let maxDisponible = Math.min(cantidadNecesaria, arrayTemp.length);
+
+        for (let i = 0; i < maxDisponible; i++) {
+            const randomIndex = Math.floor(Math.random() * arrayTemp.length);
+            valoresDeLasCartas.push(arrayTemp[randomIndex]);
+            arrayTemp.splice(randomIndex, 1);
+        }
+        return valoresDeLasCartas;
+    };
+
+
+
+    const generarMatriz = (valoresDeLasCartas, size = 4) => {
+        contenedorJuego.innerHTML = "";
+
+        let primeraMitad = [];
+        let segundaMitad = [];
+
+        valoresDeLasCartas.forEach((carta) => {
+            primeraMitad.push({ ...carta, tipo: "imagen" });
+            segundaMitad.push({ ...carta, tipo: "texto" });
+        });
+        primeraMitad.sort(() => Math.random() - 0.5);
+        segundaMitad.sort(() => Math.random() - 0.5);
+
+        let valoresDistribuidos = [...primeraMitad, ...segundaMitad];
+
+        for (let i = 0; i < size * size; i++) {
+
+            if (i === 8) {
+                contenedorJuego.innerHTML += `
+                <div class="separador"></div>`
+            }
+            const mitad = i < 8 ? "arriba" : "abajo";
+            contenedorJuego.innerHTML += `
+                <div class="card-container" data-card-value="${valoresDistribuidos[i].name}" data-mitad="${mitad}">
+                    <div class="card-before">?</div>
+                    <div class="card-after">
+                    ${valoresDistribuidos[i].tipo === "imagen"
+                    ? `<img src="${valoresDistribuidos[i].image}" class="image"/>`
+                    : `<span class="palabra">${valoresDistribuidos[i].name === "responsabilidadSocial"
                         ? "responsabilidad<br>social"
                         : valoresDistribuidos[i].name}</span>`}
+                    </div>
                 </div>
-            </div>
-            `;
-    }
+                `;
+        }
 
-    contenedorJuego.style.gridTemplateColumns = `repeat(${tamaño},auto)`;
+        contenedorJuego.style.gridTemplateColumns = `repeat(${size},auto)`;
 
-    cartas = document.querySelectorAll(".card-container");
-    cartas.forEach((card) => {
-        card.addEventListener("click", () => {
-            if (!card.classList.contains("matched") && card !== primeraCarta) {
-                const mitad = card.getAttribute("data-mitad");
-                if (!primeraCarta) {
-                    primeraCarta = card;
-                    firstCardValue = card.getAttribute("data-card-value");
-                    mitadPrimera = mitad;
-                    card.classList.add("flipped");
-                } else {
-                    if (mitad !== mitadPrimera) {
+        cartas = document.querySelectorAll(".card-container");
+        cartas.forEach((card) => {
+            card.addEventListener("click", () => {
+                if (!card.classList.contains("matched") && card !== primeraCarta) {
+                    const mitad = card.getAttribute("data-mitad");
+                    if (!primeraCarta) {
+                        primeraCarta = card;
+                        firstCardValue = card.getAttribute("data-card-value");
+                        mitadPrimera = mitad;
                         card.classList.add("flipped");
-                        contarMovimientos();
-                        seguntaCarta = card;
-                        let secondCardValue = card.getAttribute("data-card-value");
-                        if (firstCardValue == secondCardValue) {
-                            primeraCarta.classList.add("matched");
-                            seguntaCarta.classList.add("matched");
-
-                            victorias += 1;
-                            if (victorias == Math.floor(valoresDistribuidos.length / 2)) {
-                                resultado.innerHTML = `<h2>You Won</h2>
-                                        <h4>Moves: ${cantMovimientos}</h4>`;
-                                detenerJuego();
-                            }
-                        } else {
-                            let [tempFirst, tempSecond] = [primeraCarta, seguntaCarta];
-
-                            let delay = setTimeout(() => {
-                                tempFirst.classList.remove("flipped");
-                                tempSecond.classList.remove("flipped");
-                            }, 500);
-                        }
-                        primeraCarta = false;
-                        seguntaCarta = false;
-                        mitadPrimera = null;
                     } else {
-                        console.log("Debes elegir una carta de la otra mitad.");
+                        if (mitad !== mitadPrimera) {
+                            card.classList.add("flipped");
+                            contarMovimientos();
+                            seguntaCarta = card;
+                            let secondCardValue = card.getAttribute("data-card-value");
+                            if (firstCardValue == secondCardValue) {
+                                primeraCarta.classList.add("matched");
+                                seguntaCarta.classList.add("matched");
+
+                                puntos += 10;
+
+                                victorias += 1;
+                                if (victorias == Math.floor(valoresDistribuidos.length / 2)) {
+                                    resultado.innerHTML = `
+                                            <h2>¡Ganaste!</h2>
+                                            <h4>Puntaje: ${puntos}</h4>
+                                            <h4>Movimientos: ${cantMovimientos}</h4>
+                                            <h4>Tiempo: ${minutos < 10 ? "0" + minutos : minutos}:${segundos < 10 ? "0" + segundos : segundos}</h4>
+                                        `;
+                                    contenedorJuego.style.display = "none";
+                                    moves.style.display = "none";
+                                    tiempo.style.display = "none";
+                                    btnDetener.style.display = "none";
+                                    nombre.style.display = "none";
+                                    resultado.style.display = "block";
+
+                                    clearInterval(intervalo);
+
+                                    let puntosGuardados = parseInt(localStorage.getItem('puntajeJugador')) || 0;
+                                    localStorage.setItem('puntajeJugador', puntosGuardados + puntos);
+
+                                    setTimeout(() => {
+                                        window.location.href = "../pages/menuJuego.html";
+                                    }, 5000);
+                                }
+
+                            } else {
+                                let [tempFirst, tempSecond] = [primeraCarta, seguntaCarta];
+
+                                let delay = setTimeout(() => {
+                                    tempFirst.classList.remove("flipped");
+                                    tempSecond.classList.remove("flipped");
+                                }, 500);
+                            }
+                            primeraCarta = false;
+                            seguntaCarta = false;
+                            mitadPrimera = null;
+                        } else {
+                            console.log("Debes elegir una carta de la otra mitad.");
+                        }
                     }
                 }
-            }
+            });
         });
-    });
-};
+    };
 
-//Start game
-btnComenzar.addEventListener("click", () => {
-    cantMovimientos = 0;
-    segundos = 0;
-    minutos = 0;
-    //controls amd buttons visibility
-    controles.classList.add("hide");
-    btnDetener.classList.remove("hide");
-    btnComenzar.classList.add("hide");
-    //Start timer
-    intervalo = setInterval(timeGenerator, 1000);
-    //initial moves
-    moves.innerHTML = `<span>Moves:</span> ${cantMovimientos}`;
+
+    btnDetener.addEventListener("click", () => {
+        clearInterval(intervalo);
+        window.location.href = "../pages/menuJuego.html";
+    });
+
+
+    const iniciar = () => {
+        resultado.innerText = "";
+        segundos = 0;
+        minutos = 0;
+        cantMovimientos = 0;
+        puntos = 0;
+        victorias = 0;
+        intervalo = setInterval(timeGenerator, 1000);
+        moves.innerHTML = `<span>Movimientos:</span> ${cantMovimientos}`;
+        let valoresDeLasCartas = generateRandom();
+        generarMatriz(valoresDeLasCartas);
+    };
+
     iniciar();
 });
-
-//Stop game
-btnDetener.addEventListener(
-    "click",
-    (detenerJuego = () => {
-        controles.classList.remove("hide");
-        btnDetener.classList.add("hide");
-        btnComenzar.classList.remove("hide");
-        clearInterval(intervalo);
-    })
-);
-
-//Initialize values and func calls
-const iniciar = () => {
-    resultado.innerText = "";
-    victorias = 0;
-    let valoresDeLasCartas = generateRandom();
-    console.log(valoresDeLasCartas);
-    generarMatriz(valoresDeLasCartas);
-};
-
-document.getElementById("back").addEventListener("click", () => {
-    window.location.href = "../pages/menuJuego.html";
-}); 
